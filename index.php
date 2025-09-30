e164d48e33d9f5f7b377a97a5798d3756b3372af
Anthony-EMILE-GUILLOT <a.emileguillot@gmail.com>
2025-09-30 11:05:42 +0200

    fix

--- index.php
+++ index.php
@@ -4,4 +4,5 @@ include 'film.php';
 
 $premierFilm = new Film(1, "Bac Nord", 563921);
 
-echo "Infos sur le film ==>\n" . $premierFilm->getIdFilm() . " - " . $premierFilm->getNomFilm() . " - " . $premierFilm->getNdEntrees() . "\n";
+echo "Infos sur le film ==>\n" . $premierFilm->getIdFilm()." - ".$premierFilm->getNomFilm()." - ".$premierFilm->getNdEntrees()."\n";
+
