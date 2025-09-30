37c7d36bcb55250671afc64b793685aab2f3960f
Anthony-EMILE-GUILLOT <a.emileguillot@gmail.com>
2025-09-30 11:57:44 +0200

    fix

--- index.php
+++ index.php
@@ -4,5 +4,14 @@ include 'film.php';
 
 $premierFilm = new Film(1, "Bac Nord", 563921);
 
-echo "Infos sur le film ==>\n" . $premierFilm->getIdFilm()." - ".$premierFilm->getNomFilm()." - ".$premierFilm->getNdEntrees()."\n";
+//echo "Infos sur le film ==>\n" . $premierFilm->getIdFilm()." - ".$premierFilm->getNomFilm()." - ".$premierFilm->getNdEntrees()."\n";
 
+$secondFilm = new Film(2, "Boîte noire", 432789);
+$troisiemeFilm = new Film(3, "The Batman", 879214);
+
+$listeFilm = array($premierFilm, $secondFilm, $troisiemeFilm);
+$nbLigneTab = count($listeFilm);
+for($listCount=0; $listCount < $nbLigneTab; $listCount++)
+    {
+        echo "Infos sur le film ==>\n" . $listeFilm[$listCount]->getIdFilm()." - ".$listeFilm[$listCount]->getNomFilm()." - ".$listeFilm[$listCount]->getNdEntrees()."\n";
+    }
