1835a4d137fb11d32e7f4876eadfe2b96859f2ea
Anthony-EMILE-GUILLOT <a.emileguillot@gmail.com>
2025-10-07 10:32:55 +0200

    update acteur.php

--- acteur.php
+++ acteur.php
@@ -5,4 +5,41 @@ class Acteur
     private string $prenom;
     private string $nom;
     private bool $estCesarise;
+
+    public function __construct(string $prenom, string $nom, bool $estCesarise)
+    {
+        $this->prenom = $prenom;
+        $this->nom = $nom;
+        $this->estCesarise = $estCesarise;
+    }
+
+    public function getPrenom(): string
+    {
+        return $this->prenom;
+    }
+
+    public function getNom(): string
+    {
+        return $this->nom;
+    }
+
+    public function isEstCesarise(): bool
+    {
+        return $this->estCesarise;
+    }
+
+    public function setPrenom(string $prenom): void
+    {
+        $this->prenom = $prenom;
+    }
+
+    public function setNom(string $nom): void
+    {
+        $this->nom = $nom;
+    }
+
+    public function setEstCesarise(bool $estCesarise): void
+    {
+        $this->estCesarise = $estCesarise;
+    }
 }
